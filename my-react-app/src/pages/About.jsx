debd3b807271c6a34011818e7fc7b9b07f7b4f4e
user1 <user01@em/com>
2025-05-21 14:28:27 +0300

    c6

--- my-react-app/src/pages/About.jsx
+++ my-react-app/src/pages/About.jsx
@@ -3,14 +3,14 @@ import React from "react";
 function About() {
   return <div  className="flex justify-center py-5">
     <div className="w-8/10">
-    <h1>What do we offer?</h1>
+    <h1 className="bg-blue-600 text-white p-3 rounded">What do we offer?</h1>
     <ul className="flex gap-3 my-3">
-      <li>Bootcamps</li>
-      <li>Training programs</li>
-      <li>E-Learning</li>
-      <li>Global competitions</li>
-      <li>Digital communities</li>
-      <li>Knowledge sessions</li>
+      <li className="bg-black text-white p-3 rounded">Bootcamps</li>
+      <li className="bg-black text-white p-3 rounded">Training programs</li>
+      <li className="bg-black text-white p-3 rounded">E-Learning</li>
+      <li className="bg-black text-white p-3 rounded">Global competitions</li>
+      <li className="bg-black text-white p-3 rounded">Digital communities</li>
+      <li className="bg-black text-white p-3 rounded">Knowledge sessions</li>
     </ul>
   </div>;
   </div>
